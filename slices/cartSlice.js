28eed4b38bd90bc9d8937d71ebb9c1aaa90e772b
Marcin Topolski <marcin.topolski96@gmail.com>
2023-02-17 19:18:56 +0100

    Reset data

--- slices/cartSlice.js
+++ slices/cartSlice.js
@@ -1,18 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 initialState = {
-  items: [
-    {
-      id: 1,
-      imgUrl:
-        "https://ecsmedia.pl/c/harry-potter-i-zakon-feniksa-tom-5-w-iext123071903.jpg",
-      title: "Harry Potter i Zakon Feniksa",
-      author: "J.K. Rowling",
-      category: "Fantastyka",
-      description:
-        "Harry znów spędza nudne, samotne wakacje w domu Dursleyów. Czeka go piąty rok nauki w Hogwarcie i chciałby jak najszybciej spotkać się ze swoimi najlepszymi przyjaciółmi, Ronem i Hermioną. Ci jednak wyraźnie go zaniedbują. Gdy Harry ma już dość wszystkiego i postanawia zmienić swoją nieznośną sytuację, sprawy przyjmują całkiem nieoczekiwany obrót. Wygląda na to, że nowy rok nauki w Hogwarcie będzie bardzo dramatyczny. Po raz pierwszy w życiu Harry poczuje się tam nie jak w domu, tylko… więzieniu, i to nie za sprawą przywróconego do życia Lorda Voldemorta. ",
-    },
-  ],
+  items: [],
   removedItem: [],
   deliveryPoint: [],
 };
